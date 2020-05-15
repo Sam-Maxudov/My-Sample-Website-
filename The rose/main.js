8910c1ab21be8f73559944a8b092d9bf0427cb82
Sardorbek Maksudov <63782463+Sam-Maxudov@users.noreply.github.com>
2020-05-15 10:27:01 +0400

    Add files via upload

---
+++ The rose/main.js
@@ -0,0 +1,38 @@
+const btn = document.querySelector('.menu-toggle');
+const body = document.querySelector('body')
+
+btn.addEventListener('click', () => {
+    body.classList.toggle('open');
+})
+
+
+// Scroll reveal
+window.sr = ScrollReveal();
+
+sr.reveal('.animate-left', {
+    origin: 'left',
+    duration: 1000,
+    distance: '25rem',
+    delay: 300
+});
+
+sr.reveal('.animate-right', {
+    origin: 'right',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-top', {
+    origin: 'top',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
+
+sr.reveal('.animate-bottom', {
+    origin: 'bottom',
+    duration: 1000,
+    distance: '25rem',
+    delay: 600
+});
